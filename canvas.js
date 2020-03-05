8fbf055ce0f1874648c228838e19108de4b99d71
Syed Salman <37833818+alphamod@users.noreply.github.com>
2020-03-05 14:09:33 +0530

    updated canvas

--- canvas.js
+++ canvas.js
@@ -18,35 +18,45 @@ let currY;
 document.getElementById('leftBtn').disabled = true;
 document.getElementById('rightBtn').disabled = true;
 document.getElementById('deleteBtn').disabled = true;
+isDrawn= false;
 
-
-let draw = (callback) => {
-    let { x, y, r, g, b } = callback();
+let draw = (x,y,upX,upY) => {
+    // let { x, y, r, g, b } = callback();
+    r = 255;
+    g = 0;
+    b = 0;
     c.fillStyle = `rgb(${r},${g},${b})`;
-    c.fillRect(x, y, 200, 100);
-    c.stroke();
+    if (selectedObject=="rect"){
+        c.fillRect(x, y, upX, upY);
+        c.stroke();
+    } else if( selectedObject == "circle"){
+        // c.arc(x,y,())
+    }
     document.getElementById('drawBtn').disabled = true;
     document.getElementById('leftBtn').disabled = false;
     document.getElementById('rightBtn').disabled = false;
     document.getElementById('deleteBtn').disabled = false;
     currX = x;
     currY = y;
+    height = upY;
+    width = upX;
+    isDrawn = true;
 }
-let shiftRight = () => {
+let shiftRight = (width, height) => {
     if (currX + 200 < 1095) {
         currX += 15;
         c.clearRect(0, 0, innerWidth, innerHeight);
-        c.fillRect(currX, currY, 200, 100);
+        c.fillRect(currX, currY, width, height);
     } else {
         // console.log(currX+200)
         alert('Reached Border!! Shift Left or Delete.')
     }
 }
-let shiftLeft = () => {
+let shiftLeft = (upX, upY) => {
     if (currX > 5) {
         currX -= 15;
         c.clearRect(0, 0, innerWidth, innerHeight);
-        c.fillRect(currX, currY, 200, 100);
+        c.fillRect(currX, currY, upX, upY);
     } else {
         alert('Reached Border!! Shift Right or Delete.')
     }
@@ -58,5 +68,34 @@ let deleteRect = () => {
     document.getElementById('leftBtn').disabled = true;
     document.getElementById('rightBtn').disabled = true;
     document.getElementById('deleteBtn').disabled = true;
+    isDrawn=false;
 }
 
+let selectObject = (event)=>{
+     selectedObject = event.target.value;
+     console.log(selectedObject);
+}
+
+let getMousePosition= ()=>{
+
+}
+
+ canvas.addEventListener('mousedown', (event)=>{
+     x = event.clientX - event.target.offsetLeft;
+     y = event.clientY - event.target.offsetTop;
+     // console.log(event.target.offsetLeft, event.target.offsetTop);
+     // console.log(event.clientX, event.clientY);
+     console.log(x, y);
+     // draw(x,y);
+     // draw(vars);
+    })
+    
+    canvas.addEventListener('mouseup', (event)=>{
+        upX = (event.clientX - event.target.offsetLeft)-x;
+        upY = (event.clientY - event.target.offsetTop)-y;   
+    console.log(upX, upY);
+    if(isDrawn==false){
+        draw(x,y,upX,upY);
+    }
+    // console.log(event)
+})
